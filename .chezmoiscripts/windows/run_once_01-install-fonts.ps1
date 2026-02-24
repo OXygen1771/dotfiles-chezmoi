cc033350b7308c425ba399b131bd4577c3d7a55b
oxygen1771 <official.oxygen.1771@gmail.com>
2026-02-24 17:39:24 +0300

    fix(windows): check if font is already installed before installing it

--- .chezmoiscripts/windows/run_once_01-install-fonts.ps1
+++ .chezmoiscripts/windows/run_once_01-install-fonts.ps1
@@ -1,6 +1,6 @@
 $fonts = @(
     "JetBrainsMono",
-    "NerdFontsSymbolsOnly"
+    "NerdFontsSymbolsOnlyMono"
 )
 
 Write-Host "NerdFonts PowerShell module needed to install fonts. Trying to install..." -ForegroundColor Cyan
@@ -10,6 +10,13 @@ Write-Host "NerdFonts module installed!" -ForegroundColor Green
 
 foreach ($font in $fonts)
 {
+    $fontExists = (New-Object System.Drawing.Text.InstalledFontCollection).Families | Where-Object {($_.Name -replace '\s', '') -eq $font}
+    if ($fontExists)
+    {
+        Write-Host "Font $font is already installed, skipping" -ForegroundColor Green
+	continue
+    }
+
     Write-Host "Installing font $font" -ForegroundColor Cyan
     Install-NerdFont -Name $font
     Write-Host "Font $font installed successfully!" -ForegroundColor Green
