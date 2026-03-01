43d30e3c9634f37551b16deaa08b99aecb11fd27
oxygen <official.oxygen.1771@gmail.com>
2026-03-01 18:48:24 +0300

    refactor(windows/fonts): check if font installer is available
    Check if NerdFonts font installer is available before installing fonts.
    
    Clean font installation check a bit.

--- .chezmoiscripts/windows/run_once_01-install-fonts.ps1
+++ .chezmoiscripts/windows/run_once_01-install-fonts.ps1
@@ -3,23 +3,40 @@ $fonts = @(
     "NerdFontsSymbolsOnlyMono"
 )
 
-Write-Host "NerdFonts PowerShell module needed to install fonts. Trying to install..." -ForegroundColor Cyan
-Install-PSResource -Name NerdFonts
+if (!(Get-Module -ListAvailable -Name NerdFonts))
+{
+    Write-Host "NerdFonts PowerShell module needed to install fonts. Trying to install..." -ForegroundColor Cyan
+    
+    if (!(Get-Command Install-PSResource -ErrorAction SilentlyContinue))
+    {
+        Install-Module -Name Microsoft.PowerShell.PSResourceGet -Force -Scope CurrentUser
+    }
+    Install-PSResource -Name NerdFonts
+    Write-Host "NerdFonts module installed!" -ForegroundColor Green
+}
+
 Import-Module -Name NerdFonts
-Write-Host "NerdFonts module installed!" -ForegroundColor Green
+
+$installedFonts = (New-Object System.Drawing.Text.InstalledFontCollection).Families.Name
 
 foreach ($font in $fonts)
 {
-    $fontExists = (New-Object System.Drawing.Text.InstalledFontCollection).Families | Where-Object {($_.Name -replace '\s', '') -eq $font}
-    if ($fontExists)
+    if ($installedFonts -contains $font -or $installedFonts -replace '\s', '' -contains $font)
     {
         Write-Host "Font $font is already installed, skipping" -ForegroundColor Green
-	continue
+        continue
     }
 
     Write-Host "Installing font $font" -ForegroundColor Cyan
-    Install-NerdFont -Name $font
-    Write-Host "Font $font installed successfully!" -ForegroundColor Green
+    try
+    {
+        Install-NerdFont -Name $font
+        Write-Host "Font $font installed successfully!" -ForegroundColor Green
+    }
+    catch
+    {
+        Write-Host "Failed to install $font : $($_.Exception.Message)" -ForegroundColor Red
+    }
 }
 
 Write-Host "All fonts installed successfully!" -ForegroundColor Green
